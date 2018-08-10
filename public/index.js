a55d16160e798d0872df18f4bbc1ce8cdbad1490
Kole <kole6393@gmail.com>
2018-08-10 11:38:42 -0500

    use es6 fetch

--- public/index.js
+++ public/index.js
@@ -1,27 +1,8 @@
-const ajax = function(method, url, data, callback) {
-  try {
-    var x = new(XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0');
-    x.open(method, url, 1);
-    x.withCredentials = true;
-    x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-    x.setRequestHeader('Content-type', 'application/json');
-    x.onreadystatechange = function () {
-      x.readyState > 3 && callback && callback(x.responseText, x);
-    };
-    x.send(data)
-  } catch (e) {}
-};
-
-const el = function(id) {
-  return document.getElementById(id);
-}
-
 let music;
 
 document.addEventListener('musickitloaded', function() {
   // MusicKit global is now defined
-  ajax('get', '/token', {}, function(res) {
-    res = JSON.parse(res);
+  fetch('/token').then(response => response.json()).then((res) => {
     MusicKit.configure({
       developerToken: res.token,
       app: {
@@ -32,14 +13,16 @@ document.addEventListener('musickitloaded', function() {
 
     music = MusicKit.getInstance();
 
+    const el = function(id) {
+      return document.getElementById(id);
+    }
+
     el("add-to-q-btn").addEventListener("click", function(){
       const idInput   = el('id-input');
       const typeInput = el('type-input');
-      const id        = idInput.value;
-      const type      = typeInput.value;
 
       music.setQueue({
-        [type]: id
+        [typeInput.value]: idInput.value
       });
 
       idInput.value   = "";
