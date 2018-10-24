2f0229a97727e269377907b9c4d785def731df98
Kole <kole6393@gmail.com>
2018-10-24 10:45:06 -0500

    add login button

--- public/index.js
+++ public/index.js
@@ -1,5 +1,37 @@
 let music;
 
+function getEl(id) {
+  return document.getElementById(id);
+}
+
+function setupClickListeners() {
+  getEl('add-to-q-btn').addEventListener('click', () => {
+    const idInput   = getEl('id-input');
+    const typeInput = getEl('type-input');
+
+    music.setQueue({
+      [typeInput.value]: idInput.value
+    });
+
+    idInput.value   = '';
+    typeInput.value = '';
+  });
+
+  getEl('play-btn').addEventListener('click', () => {
+    music.play();
+  });
+
+  getEl('pause-btn').addEventListener('click', () => {
+    music.pause();
+  });
+
+  getEl('login-btn').addEventListener('click', () => {
+    music.authorize().then(musicUserToken => {
+      console.log(`musicUserToken = ${musicUserToken}`);
+    });
+  });
+}
+
 document.addEventListener('musickitloaded', () => {
   // MusicKit global is now defined
   fetch('/token').then(response => response.json()).then(res => {
@@ -11,28 +43,6 @@ document.addEventListener('musickitloaded', () => {
       }
     });
 
-    const getEl = (id) => {
-      return document.getElementById(id);
-    }
-
-    getEl('add-to-q-btn').addEventListener('click', () => {
-      const idInput   = getEl('id-input');
-      const typeInput = getEl('type-input');
-
-      music.setQueue({
-        [typeInput.value]: idInput.value
-      });
-
-      idInput.value   = '';
-      typeInput.value = '';
-    });
-
-    getEl('play-btn').addEventListener('click', () => {
-      music.play();
-    });
-
-    getEl('pause-btn').addEventListener('click', () => {
-      music.pause();
-    });
+    setupClickListeners();
   });
 });
